21aa51309d3cfa190c2e047e824203ff22b2f48c
Fernando Ximenes <fxdias@gmail.com>
2022-10-20 09:58:23 -0300

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Header.module.css'
+import { Link } from 'react-router-dom'
+
+function Header() {
+    return (
+        <div className="styles.header">
+            <nav>
+                <Link to="/">Login / Criar</Link>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
